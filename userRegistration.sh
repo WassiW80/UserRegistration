c7c79fa8e70c6a78267950482e29f80252338d1f
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-14 16:41:23 +0530

    ADDED: Validating mobile number.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,14 +1,20 @@
 #!/bin/bash 
 shopt -s extglob
 echo "Welcome to User Registration."
+#reading values from user
 read -p "Enter First name minimum 3 character: " firstname
 read -p "Enter Last name minimum 3 character: " lastname
 read -p "Enter email id : " email
+read -p "Enter Mobile Number : " mobileNumber
+#pattern declaration for validation
 emailPattern="^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}([.][A-Za-z]{2,4}+)?$"
 namePattern="^[A-Z]{1}[a-z]{2,}$"
+mobilePattern="^([0-9]{1,3}[ ][0-9]{10})$"
+#name of pattern
 name="firstname"
 name2="lastname"
 name3="email"
+name4="mobile"
 function validateUserData() {
 	if [[ $1 =~ $3 ]]
 	then
@@ -20,3 +26,6 @@ function validateUserData() {
 validateUserData $firstname $name $namePattern
 validateUserData $lastname $name2 $namePattern
 validateUserData $email $name3 $emailPattern
+validateUserData "$mobileNumber" $name4 "$mobilePattern"
+
+
