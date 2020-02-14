998bef089b4880cf4daaa5e0ebfd226dba038cf1
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-14 11:35:57 +0530

    ADDED: Validating first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,1 +1,14 @@
+#!/bin/bash -x
+shopt -s extglob
 echo "Welcome to User Registration."
+function validateFirstname() {
+	read -p "Enter user name minimum 3 character: " firstname
+	firstnamePattern="^[A-Z]{1}[A-Za-z]{2,}$"
+	if [[ $firstname =~ $firstnamePattern ]]
+	then
+		echo valid
+	else
+		echo invalid
+	fi
+}
+validateFirstname
