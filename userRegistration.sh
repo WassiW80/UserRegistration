7014acfa72a9b9bb1b9e6b69084237f96f441270
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-14 13:11:14 +0530

    ADDED: Validating email address.

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,16 +3,20 @@ shopt -s extglob
 echo "Welcome to User Registration."
 read -p "Enter First name minimum 3 character: " firstname
 read -p "Enter Last name minimum 3 character: " lastname
+read -p "Enter email id : " email
+emailPattern="^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}([.][A-Za-z]{2,4}+)?$"
+namePattern="^[A-Z]{1}[a-z]{2,}$"
 name="firstname"
 name2="lastname"
-function validateFirstname() {
-	namePattern="^[A-Z]{1}[a-z]{2,}$"
-	if [[ $1 =~ $namePattern ]]
+name3="email"
+function validateUserData() {
+	if [[ $1 =~ $3 ]]
 	then
 		echo "$2 valid"
 	else
 		echo "$2 invalid"
 	fi
 }
-validateFirstname $firstname $name
-validateFirstname $lastname $name2
+validateUserData $firstname $name $namePattern
+validateUserData $lastname $name2 $namePattern
+validateUserData $email $name3 $emailPattern
